2bcf5f975f3db2fdb2f765d3537b168280189719
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-09-06 19:02:13 +0200

    feat(model): Add SetFirstOpenToNow in Config

--- model/config.go
+++ model/config.go
@@ -2,11 +2,17 @@ package model
 
 import (
 	"gorm.io/gorm"
+	"time"
 )
 
 type Config struct {
 	gorm.Model
 	TextEditor         string `json:"string_editor"`
 	ServerPort         uint   `json:"server_port"`
-	FirstOpenTimestamp uint   `json:"first_open_timestamp"`
+	FirstOpenTimestamp int64  `json:"first_open_timestamp"`
+}
+
+func (c *Config) SetFirstOpenToNow() {
+	currentUnixTimestamp := time.Now().Unix()
+	c.FirstOpenTimestamp = currentUnixTimestamp
 }

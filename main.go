5c5d66c5d3b24db686b172f1176144e9ea04560d
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-09-06 18:38:31 +0200

    feat(main): Add database initialization

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "DeployX/cmd"
+import (
+	"DeployX/cmd"
+	"DeployX/models"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func initializeDatabase() {
+	db, err := gorm.Open(sqlite.Open("deployx.db"), &gorm.Config{})
+	if err != nil {
+		panic("Can't open database")
+	}
+
+	err = db.AutoMigrate(&models.Config{})
+	if err != nil {
+		panic("Can't migrate Config")
+	}
+}
 
 func main() {
+	initializeDatabase()
 	cmd.Execute()
 }

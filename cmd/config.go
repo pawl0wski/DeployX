29e9be1cb14d7eb6bb2a7b599212cc4ee0f319e3
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-09-06 20:10:21 +0200

    refactor(cmd): Create runCommand function in config.go

--- cmd/config.go
+++ cmd/config.go
@@ -2,7 +2,6 @@ package cmd
 
 import (
 	"fmt"
-
 	"github.com/spf13/cobra"
 )
 
@@ -10,21 +9,13 @@ import (
 var configCmd = &cobra.Command{
 	Use:   "config",
 	Short: "Configure DeployX",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("config called")
-	},
+	Run:   runCommand,
+}
+
+func runCommand(cmd *cobra.Command, args []string) {
+	fmt.Println("Welcome in DeployX configuration! 🛠")
 }
 
 func init() {
 	rootCmd.AddCommand(configCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// configCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// configCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }

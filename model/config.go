a30f6cf300a70831c48ec4742387cb93905b8636
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-09-06 20:56:58 +0200

    refactor(cmd): Remove FirstConfigurationTimestamp from Config

--- model/config.go
+++ model/config.go
@@ -2,21 +2,10 @@ package model
 
 import (
 	"gorm.io/gorm"
-	"time"
 )
 
 type Config struct {
 	gorm.Model
-	TextEditor                  string `json:"string_editor"`
-	ServerPort                  uint   `json:"server_port"`
-	FirstConfigurationTimestamp int64  `json:"first_configuration_timestamp"`
-}
-
-func (c *Config) SetFirstConfigurationToNow() {
-	currentUnixTimestamp := time.Now().Unix()
-	c.FirstConfigurationTimestamp = currentUnixTimestamp
-}
-
-func (c *Config) WasConfiguredBefore() bool {
-	return c.FirstConfigurationTimestamp != 0
+	TextEditor string `json:"string_editor"`
+	ServerPort uint   `json:"server_port"`
 }

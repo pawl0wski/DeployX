11580b04fb9157e584f8f2f3c568c1acd563d0f1
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-09-08 18:15:23 +0200

    fetch(model): Add Update and Refresh methods to Config struct

--- model/config.go
+++ model/config.go
@@ -1,6 +1,7 @@
 package model
 
 import (
+	"DeployX/database"
 	"gorm.io/gorm"
 )
 
@@ -8,4 +9,13 @@ type Config struct {
 	gorm.Model
 	TextEditor string `json:"string_editor"`
 	ServerPort uint   `json:"server_port"`
+}
+
+func (config *Config) Update() {
+	database.DBConn.Updates(config)
+}
+
+// Refresh Config with values from database
+func (config *Config) Refresh() {
+	database.DBConn.FirstOrCreate(config)
 }

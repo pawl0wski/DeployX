b5188808649b2cf85002bb527e443671520a75cf
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-09-09 19:51:11 +0200

    test(modules): Implement test for HashPassword

---
+++ modules/hash_password_test.go
@@ -0,0 +1,14 @@
+package modules_test
+
+import (
+	"DeployX/modules"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestHashPassword(t *testing.T) {
+	passwordToHash := "ABC"
+	expectedOutput := "b5d4045c3f466fa91fe2cc6abe79232a1a57cdf104f7a26e716e0a1e2789df78"
+
+	assert.Equal(t, expectedOutput, modules.HashPassword(passwordToHash))
+}

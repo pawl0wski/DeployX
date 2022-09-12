49e1a5ab91d36ab8b86195e0b7adf475f2649776
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-09-12 22:19:31 +0200

    refactor(prompts): Move SelectProject method from cmd to prompts

---
+++ prompts/select_project.go
@@ -0,0 +1,32 @@
+package prompts
+
+import (
+	"DeployX/models"
+	"github.com/AlecAivazis/survey/v2"
+	"github.com/fatih/color"
+	"os"
+)
+
+func SelectProject() *models.Project {
+	projects := models.GetAllProjects()
+	if len(projects) == 0 {
+		color.Red("You don't have any projects. Make one first.")
+		os.Exit(0)
+	}
+	var promptOptions []string
+	for _, project := range projects {
+		promptOptions = append(promptOptions, project.Name)
+	}
+	prompt := &survey.Select{Message: "Pick project", Options: promptOptions}
+	var selectedProjectName string
+	err := survey.AskOne(prompt, &selectedProjectName)
+	if err != nil {
+		panic("Can't select project from user")
+	}
+	for _, project := range projects {
+		if project.Name == selectedProjectName {
+			return &project
+		}
+	}
+	panic("Selected unknown project name")
+}

b6db70f37dc782d6b45f1009416ca68f36e303e6
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-09-13 17:48:47 +0200

    style(cmd): Remove comments in root.go

--- cmd/root.go
+++ cmd/root.go
@@ -12,8 +12,6 @@ var rootCmd = &cobra.Command{
 	Short: "Deploy your applications quickly",
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {

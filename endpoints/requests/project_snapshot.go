c0670873d31ddd1379570463ced5348319a65f43
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-09-16 19:48:24 +0200

    feat(endpoints): Add ConvertDataToBinary in ProjectSnapshotRequest

--- endpoints/requests/project_snapshot.go
+++ endpoints/requests/project_snapshot.go
@@ -1,8 +1,19 @@
 package requests
 
+import "encoding/base64"
+
 type ProjectSnapshotRequest struct {
 	ProjectID       string `json:"project_id"`
 	ProjectPassword string `json:"project_password"`
 	Version         string `json:"snapshot_version"`
 	Data            string `json:"data"`
+}
+
+func (request *ProjectSnapshotRequest) ConvertDataToBinary() []byte {
+	encoder := base64.Encoding{}
+	binaryData, err := encoder.DecodeString(request.Data)
+	if err != nil {
+		panic("Can't covert data to binary")
+	}
+	return binaryData
 }

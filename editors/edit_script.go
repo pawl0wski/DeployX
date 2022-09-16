224be2c513517ad4815dbbd3827596a8a71ad004
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-09-16 23:45:52 +0200

    refactor(editors): Use editor from "survey" package in EditScript

--- editors/edit_script.go
+++ editors/edit_script.go
@@ -1,26 +1,14 @@
 package editors
 
 import (
-	"fmt"
+	"github.com/AlecAivazis/survey/v2"
 	"github.com/pawl0wski/DeployX/models"
-	"github.com/pawl0wski/DeployX/temp"
-	"os"
-	"os/exec"
 )
 
 func EditScript(script *models.Script, textEditor string) {
-	file := temp.FileCreator(script.Content)
-	textEditorCommand := prepareTextEditorToEditScript(textEditor, file)
-	err := textEditorCommand.Run()
+	editor := &survey.Editor{Editor: textEditor, FileName: "*.sh", Default: script.Content, HideDefault: false, AppendDefault: true}
+	err := survey.AskOne(editor, &script.Content)
 	if err != nil {
-		panic(fmt.Sprintf("Can't execute %s", textEditor))
+		panic(err)
 	}
-	script.Content = temp.GetContentFromTempFile(file)
-}
-
-func prepareTextEditorToEditScript(textEditor string, file *os.File) *exec.Cmd {
-	textEditorCommand := exec.Command(textEditor, file.Name())
-	textEditorCommand.Stdin = os.Stdin
-	textEditorCommand.Stdout = os.Stdout
-	return textEditorCommand
 }

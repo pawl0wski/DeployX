9e5729b52a3f292693ce8aceee6ce2ca1a6aae4b
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-09-16 20:47:01 +0200

    feat(models): Add GetLastSnapshotVersionByProjectID and Save in ProjectSnapshot

--- models/project_snapshot.go
+++ models/project_snapshot.go
@@ -3,6 +3,7 @@ package models
 import (
 	"github.com/pawl0wski/DeployX/database"
 	"gorm.io/gorm"
+	"strconv"
 )
 
 type ProjectSnapshot struct {
@@ -12,6 +13,16 @@ type ProjectSnapshot struct {
 	CurrentSnapshot bool    `json:"current_snapshot"`
 	Project         Project `json:"project"`
 	ProjectID       int     `json:"project_id"`
+}
+
+func (snapshot *ProjectSnapshot) Save() {
+	database.DBConn.Save(snapshot)
+}
+
+func GetLastSnapshotVersionByProjectID(id int) int {
+	var snapshot ProjectSnapshot
+	database.DBConn.Where("project_id = ?", []string{strconv.Itoa(int(id))}).Order("version desc").First(&snapshot)
+	return snapshot.Version
 }
 
 func InitializeProjectSnapshot() {

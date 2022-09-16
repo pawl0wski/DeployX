d3dec52e8790731ffd20b925ec2abb1587a0e86c
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-09-16 20:47:47 +0200

    feat(endpoints): Add PostProjectSnapshot in Engine

--- endpoints/get_version.go
+++ endpoints/get_version.go
@@ -5,7 +5,7 @@ import (
 	"github.com/pawl0wski/DeployX/endpoints/responses"
 )
 
-func Version(c *gin.Context) {
+func GetVersion(c *gin.Context) {
 	response := responses.VersionResponse{Version: "1.0.0", Name: "DeployX"}
 	c.JSON(200, &response)
 }
--- endpoints/initialize_endpoints.go
+++ endpoints/initialize_endpoints.go
@@ -3,5 +3,6 @@ package endpoints
 import "github.com/gin-gonic/gin"
 
 func InitializeEndpoints(engine *gin.Engine) {
-	engine.GET("/version", Version)
+	engine.GET("/version", GetVersion)
+	engine.POST("/project_snapshot", PostProjectSnapshot)
 }

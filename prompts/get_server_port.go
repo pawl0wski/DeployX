d2af946538d40ab4be33dc04e2c4861ac9108a2c
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-09-20 22:45:59 +0200

    refactor(prompts): Organize code in GetServerPortPrompt

--- prompts/get_server_port.go
+++ prompts/get_server_port.go
@@ -14,16 +14,9 @@ type GetServerPortPrompt struct {
 func (p *GetServerPortPrompt) Run() uint16 {
 	defaultPort := p.getDefaultPort()
 	prompt := &survey.Input{Message: "Server port", Default: strconv.Itoa(int(defaultPort))}
-	var serverPortAsString string
-	err := survey.AskOne(prompt, &serverPortAsString, survey.WithValidator(p.validateServerPort))
-	if err != nil {
-		panic("Can't get server port")
-	}
-	port, err := strconv.ParseUint(serverPortAsString, 10, 16)
-	if err != nil {
-		panic("Invalid port")
-	}
-	return uint16(port)
+	response := p.askUser(prompt)
+	port := p.convertResponseToUInt16(response)
+	return port
 }
 
 func (p *GetServerPortPrompt) getDefaultPort() uint16 {
@@ -31,6 +24,27 @@ func (p *GetServerPortPrompt) getDefaultPort() uint16 {
 		return 7777
 	}
 	return p.DefaultPort
+}
+
+func (p *GetServerPortPrompt) preparePrompt(defaultPort uint16) *survey.Input {
+	return &survey.Input{Message: "Server port", Default: strconv.Itoa(int(defaultPort))}
+}
+
+func (p *GetServerPortPrompt) askUser(prompt *survey.Input) string {
+	var response string
+	err := survey.AskOne(prompt, &response, survey.WithValidator(p.validateServerPort))
+	if err != nil {
+		panic("Can't get server port")
+	}
+	return response
+}
+
+func (p *GetServerPortPrompt) convertResponseToUInt16(response string) uint16 {
+	port, err := strconv.ParseUint(response, 10, 16)
+	if err != nil {
+		panic("Invalid port")
+	}
+	return uint16(port)
 }
 
 func (p *GetServerPortPrompt) validateServerPort(val interface{}) error {

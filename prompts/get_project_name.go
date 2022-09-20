60f5bea1858d9f56454792d7d79ad76a0c85bc3f
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-09-20 22:55:12 +0200

    refactor(prompts): Organize code in GetProjectNamePrompt

--- prompts/get_project_name.go
+++ prompts/get_project_name.go
@@ -9,7 +9,15 @@ type GetProjectNamePrompt struct {
 }
 
 func (p *GetProjectNamePrompt) Run() string {
-	prompt := &survey.Input{Message: "Name", Default: p.DefaultProjectName}
+	prompt := p.preparePrompt()
+	return p.askUser(prompt)
+}
+
+func (p *GetProjectNamePrompt) preparePrompt() *survey.Input {
+	return &survey.Input{Message: "Name", Default: p.DefaultProjectName}
+}
+
+func (p *GetProjectNamePrompt) askUser(prompt *survey.Input) string {
 	var name string
 	err := survey.AskOne(prompt, &name)
 	if err != nil {
